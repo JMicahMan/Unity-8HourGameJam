30971b61d3e5ee8b9e0ae679c7b6a2aa776455de
JMicahMan <j.micah.manzanares@gmail.com>
2019-05-20 14:00:49 -0500

    Gameplay loop complete, onto UI

---
+++ Assets/Scripts/PikminBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PikminBehaviour : MonoBehaviour {
+
+    public float mov = 10.0f;
+	void Update () 
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            Vector3 vector3 = transform.position;
+
+            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+                
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            Vector3 vector3 = transform.position;
+
+            transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScoreBehaviour : MonoBehaviour {
+
+    public int Score;
+
+   // public GameObject knightBowlingPin;
+	// Use this for initialization
+	void Start () 
+    {
+        Score = 0;
+	}
+
+   
+}
+
